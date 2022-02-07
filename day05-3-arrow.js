822a05865dcc60b4b0c434cb3f6b7e6dd94099b7
Flavia Narumi Nitto <85723447+flavianitto@users.noreply.github.com>
2022-02-07 10:47:28 -0300

    arrow functions

---
+++ day05-3-arrow.js
@@ -0,0 +1,53 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    const double = elem => elem*2;
+    const triple = elem => elem*3;
+    let res = []; 
+
+    for(let n of nums) {
+        if(n%2 == 0) {
+            res.push(double(n));
+        }
+        else {
+            res.push(triple(n));
+        }
+    }
+    return res;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+    
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}
